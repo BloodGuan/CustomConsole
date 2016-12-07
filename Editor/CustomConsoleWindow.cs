d30b985e5c492c872a8c0561fa4dcf23500e54d5
bingxia.guan <bingxia.guan@mokun.net>
2016-12-07 17:32:39 +0800

    堆栈信息处理

--- Editor/CustomConsoleWindow.cs
+++ Editor/CustomConsoleWindow.cs
@@ -7,6 +7,8 @@ using System;
 
 public class CustomConsoleWindow : EditorWindow
 {
+    public static List<LogEntry> logEntries = new List<LogEntry>();
+
     private static CustomConsoleWindow _instance;
     public static CustomConsoleWindow instance
     {
@@ -23,86 +25,164 @@ public class CustomConsoleWindow : EditorWindow
     [MenuItem("Custom/Console")]
     public static void OpenWindow()
     {
+        string log = "+";
+        for (int i = 0; i < 10; i++)
+        {
+            log += log;
+        }
+        Debug.Log(log);
         instance.Show(true);
         instance.Focus();
     }
 
     public CustomConsoleWindow()
     {
-        logListView = new ListViewState(0, 30);
+        logListView = new ListViewState(0, 32);
     }
 
     ListViewState logListView;
     object spl = null;
     bool toggle = false;
     Vector2 textScroll = Vector2.zero;
+    string selectLogText = "";
+
     void OnGUI()
     {
+        WindowGUIStyles.Load();
         DrawToolbar();
         DrawList();
     }
     void DrawToolbar()
     {
-        GUILayout.BeginHorizontal("Toolbar");
-        GUILayout.Button("Button", "toolbarbutton");
-        toggle = GUILayout.Toggle(toggle, "Toggle", "toolbarbutton");
+        GUILayout.BeginHorizontal(WindowGUIStyles.Toolbar);
+        if (GUILayout.Button("Clear", WindowGUIStyles.ToolbarButton))
+        {
+            Clear();
+        }
+        if (GUILayout.Button("Add Log", WindowGUIStyles.ToolbarButton))
+        {
+            Debug.Log("Log");
+            LogEntry add = new LogEntry();
+            add.type = LogType.Log;
+            add.message = "Log";
+            add.stackTrace = LogEntry.GetStackTrace();
+            logEntries.Add(add);
+            Repaint();
+        }
+        if (GUILayout.Button("Add Err", WindowGUIStyles.ToolbarButton))
+        {
+            LogEntry add = new LogEntry();
+            add.type = LogType.Error;
+            add.message = "Err";
+            logEntries.Add(add);
+            Repaint();
+        }
+        if (GUILayout.Button("Add War", WindowGUIStyles.ToolbarButton))
+        {
+            LogEntry add = new LogEntry();
+            add.type = LogType.Warning;
+            add.message = "War";
+            logEntries.Add(add);
+            Repaint();
+        }
+        toggle = GUILayout.Toggle(toggle, "Toggle", WindowGUIStyles.ToolbarButton);
         GUILayout.EndHorizontal();
     }
     void DrawList()
     {
         Event current = Event.current;
-        string text = "hhhh\nhhhh\nhhhh\nhhhh\nhhhh\nhhhh\nhhhh\nhhhh\nhhhh\nhhhh\nhhhh\nhhhh\nhhhh";
-        this.logListView.totalRows = 55;
+        this.logListView.totalRows = logEntries.Count;
 
         spl = EditorDrawTools.BeginVerticalSplit(spl);
+        GUIContent logContent = new GUIContent();
         int controlID = GUIUtility.GetControlID(FocusType.Native);
-        ListViewGUI.ForeachListView(ListViewGUI.ListView(logListView.obj, "CN Box"),
+        ListViewGUI.ForeachListView(ListViewGUI.ListView(logListView.obj, WindowGUIStyles.Box),
         (listViewElement) =>
         {
             if (current.type == EventType.MouseDown && current.button == 0 && listViewElement.position.Contains(current.mousePosition))
             {
                 if (current.clickCount == 2)
                 {
-                    Debug.Log("双击了第" + logListView.row + "行");
+                    logEntries[logListView.row].GetDoubleClick();
                 }
             }
             if (current.type == EventType.Repaint)
             {
-                GUIStyle gUIStyle = (listViewElement.row % 2 != 0) ? "CN EntryBackEven" : "CN EntryBackodd";
-                gUIStyle.Draw(listViewElement.position, false, false, this.logListView.row == listViewElement.row, false);
-                GUIContent gUIContent = new GUIContent();
-                gUIContent.text = listViewElement.row.ToString() + "\n" + text;
-                GUIStyle styleForErrorMode = "CN EntryWarn";
-                styleForErrorMode.Draw(listViewElement.position, gUIContent, controlID, this.logListView.row == listViewElement.row);
+                GUIStyle backgroundStyle = (listViewElement.row % 2 != 0) ? WindowGUIStyles.LogBackground_1 : WindowGUIStyles.LogBackground_2;
+                backgroundStyle.Draw(listViewElement.position, false, false, this.logListView.row == listViewElement.row, false);
+                logContent.text = logEntries[listViewElement.row].GetTwoLine();
+                GUIStyle entryStyle = WindowGUIStyles.GetEntryStyle(logEntries[listViewElement.row].type);
+                entryStyle.Draw(listViewElement.position, logContent, controlID, this.logListView.row == listViewElement.row);
             }
             if (this.logListView.totalRows == 0 || this.logListView.row >= this.logListView.totalRows || this.logListView.row < 0)
             {
-                Debug.Log("取消选中");
+                selectLogText = "";
             }
             else
             {
-                Debug.Log("选中第" + logListView.row + "行");
+                selectLogText = logEntries[logListView.row].GetAllLine();
             }
         });
         EditorDrawTools.MidVerticalSplit();
         textScroll = GUILayout.BeginScrollView(textScroll);
-        for (int i = 0; i < 5; i++)
-        {
-            GUILayout.Label(text);
-            GUILayout.Label("--------------------------------------------------------------");
-        }
+        float minHeight = ((GUIStyle)"CN Message").CalcHeight(new GUIContent(selectLogText), base.position.width);
+        EditorGUILayout.SelectableLabel(selectLogText, "CN Message", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true), GUILayout.MinHeight(minHeight));
         GUILayout.EndScrollView();
         EditorDrawTools.EndVerticalSplit();
+    }
+
+
+    void Clear()
+    {
+        logEntries.Clear();
+        Repaint();
     }
 
 
 
 
 
+    #region GUIStyles
+    public class WindowGUIStyles
+    {
+        private static bool inited = false;
+        public static GUIStyle Toolbar;
+        public static GUIStyle ToolbarButton;
+        public static GUIStyle Box;
+        public static GUIStyle LogBackground_1;
+        public static GUIStyle LogBackground_2;
+        public static GUIStyle ErrorStyle;
+        public static GUIStyle LogStyle;
+        public static GUIStyle WarningStyle;
 
+        public static void Load()
+        {
+            if (inited) return;
+            inited = true;
 
-
-
+            Toolbar = "Toolbar";
+            ToolbarButton = "ToolbarButton";
+            Box = "CN Box";
+            LogBackground_1 = "CN EntryBackEven";
+            LogBackground_2 = "CN EntryBackodd";
+            ErrorStyle = "CN EntryError";
+            LogStyle = "CN EntryInfo";
+            WarningStyle = "CN EntryWarn";
+        }
+        public static GUIStyle GetEntryStyle(LogType type)
+        {
+            switch (type)
+            {
+                case LogType.Log:
+                    return LogStyle;
+                case LogType.Warning:
+                    return WarningStyle;
+                default:
+                    return ErrorStyle;
+            }
+        }
+    }
+    #endregion
     #region Reflection
     public class ListViewGUI
     {
@@ -396,4 +476,111 @@ public class CustomConsoleWindow : EditorWindow
         }
     }
     #endregion
+}
+public class LogEntry
+{
+    public LogType type;
+    public string message;
+    public string stackTrace;
+
+    public LogEntry()
+    {
+ 
+    }
+    public LogEntry(LogType type, string messag, string stackTrace)
+    {
+
+    }
+
+    public void GetDoubleClick()
+    {
+ 
+    }
+    public string GetTwoLine()
+    {
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+        string[] messageLines = message.Split('\n');
+        if (messageLines.Length >= 2)
+        {
+            sb.AppendLine(messageLines[0]);
+            sb.AppendLine(messageLines[1]);
+        }
+        else
+        {
+            sb.AppendLine(message);
+        }
+
+        return sb.ToString();
+    }
+    public string GetAllLine()
+    {
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+        sb.AppendLine(message);
+        sb.AppendLine(stackTrace);
+        return sb.ToString();
+    }
+
+    public static string GetStackTrace()
+    {
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+        System.Diagnostics.StackTrace st = new System.Diagnostics.StackTrace(1, true);
+        System.Diagnostics.StackFrame[] sfs = st.GetFrames();
+        
+        for (int i = 0; i < sfs.Length; i++)
+        {
+            System.Diagnostics.StackFrame item = sfs[i];
+            string fileName = item.GetFileName();
+            MethodBase method = item.GetMethod();
+            ParameterInfo[] parameters = method.GetParameters();
+            // part.1 --- 类名:方法名(参数类型)
+            sb.Append(method.ReflectedType.FullName);
+            sb.Append(":");
+            sb.Append(method.Name);
+            sb.Append("(");
+            for (int j = 0; j < parameters.Length; j++)
+            {
+                sb.Append(parameters[j].ParameterType.FullName);
+                sb.Append(" ");
+                sb.Append(parameters[j].Name);
+                if (j + 1 < parameters.Length)
+                    sb.Append(",");
+            }
+            sb.Append(")");
+            // part.1 --- end
+
+            // part.2 --- (at 路径:行数)
+            
+            if (IsFileInDataPath(fileName))
+            {
+                sb.Append(" (");
+                sb.Append("at ");
+                sb.Append(ReplaceDataPath(fileName));
+                sb.Append(":");
+                sb.Append(item.GetFileLineNumber());
+                sb.Append(")");
+            }
+            // part.2 --- end
+
+            if (i + 1 < sfs.Length)
+                sb.Append("\n");
+        }
+
+        return sb.ToString();
+    }
+    private static bool IsFileInDataPath(string fullPath)
+    {
+        if (string.IsNullOrEmpty(fullPath)) return false;
+        string dataPath = Application.dataPath;
+        dataPath = dataPath.Replace('\\', '/');
+        fullPath = fullPath.Replace('\\', '/');
+        return fullPath.StartsWith(dataPath);
+    }
+    private static string ReplaceDataPath(string fullPath)
+    {
+        if (string.IsNullOrEmpty(fullPath)) return "";
+        string dataPath = Application.dataPath;
+        dataPath = dataPath.Replace('\\', '/');
+        fullPath = fullPath.Replace('\\', '/');
+        return "Assets" + fullPath.Replace(dataPath, "");
+    }
 }
