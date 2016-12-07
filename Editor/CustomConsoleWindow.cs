acfa98138070ab9eaa0fdcae46c9df2c15f57a91
bingxia.guan <bingxia.guan@mokun.net>
2016-12-07 11:48:41 +0800

    反射性能优化

--- Editor/CustomConsoleWindow.cs
+++ Editor/CustomConsoleWindow.cs
@@ -56,7 +56,7 @@ public class CustomConsoleWindow : EditorWindow
 
         spl = EditorDrawTools.BeginVerticalSplit(spl);
         int controlID = GUIUtility.GetControlID(FocusType.Native);
-        IEnumerator vl = ListView(logListView.obj, "CN Box");
+        IEnumerator vl = ListViewGUI.ListView(logListView.obj, "CN Box");
         while (vl.MoveNext())
         {
             ListViewElement listViewElement = new ListViewElement(vl.Current);
@@ -104,20 +104,23 @@ public class CustomConsoleWindow : EditorWindow
 
 
 
-
-
-
-    public static IEnumerator ListView(object state, GUIStyle style, params GUILayoutOption[] options)
+    #region Reflection
+    public class ListViewGUI
     {
-        object obj = null;
-        Assembly editorAsm = Assembly.GetAssembly(typeof(EditorGUILayout));
-        Type ListViewGUI = editorAsm.GetType("UnityEditor.ListViewGUI");
-        Type ListViewState = editorAsm.GetType("UnityEditor.ListViewState");
-        Type[] tys = new Type[] { ListViewState, typeof(GUIStyle), typeof(GUILayoutOption[]) };
-        MethodInfo ListView = ListViewGUI.GetMethod("ListView", tys);
-        object[] args = new object[] { state, style, options };
-        obj = ListView.Invoke(null, args);
-        return (IEnumerator)obj;
+        private static MethodInfo method_ListView;
+        public static IEnumerator ListView(object state, GUIStyle style, params GUILayoutOption[] options)
+        {
+            if (method_ListView == null)
+            {
+                Assembly editorAsm = Assembly.GetAssembly(typeof(EditorGUILayout));
+                Type ListViewGUI = editorAsm.GetType("UnityEditor.ListViewGUI");
+                Type ListViewState = editorAsm.GetType("UnityEditor.ListViewState");
+                Type[] tys = new Type[] { ListViewState, typeof(GUIStyle), typeof(GUILayoutOption[]) };
+                method_ListView = ListViewGUI.GetMethod("ListView", tys);
+            }
+            object[] args = new object[] { state, style, options };
+            return (IEnumerator)method_ListView.Invoke(null, args);
+        }
     }
 
     [Serializable]
@@ -126,89 +129,161 @@ public class CustomConsoleWindow : EditorWindow
         [SerializeField]
         public object obj;
 
-        private FieldInfo _row;
-        private FieldInfo _selectionChanged;
-        private FieldInfo _totalRows;
-        private FieldInfo _scrollPos;
-        private FieldInfo _rowHeight;
-        private FieldInfo _ID;
+        private static Type m_type;
+        private static FieldInfo m_field_row;
+        private static FieldInfo m_field_selectionChanged;
+        private static FieldInfo m_field_totalRows;
+        private static FieldInfo m_field_scrollPos;
+        private static FieldInfo m_field_rowHeight;
+        private static FieldInfo m_field_ID;
+
+        private static Type type
+        {
+            get
+            {
+                if (m_type == null)
+                {
+                    Assembly editorAsm = Assembly.GetAssembly(typeof(EditorGUILayout));
+                    m_type = editorAsm.GetType("UnityEditor.ListViewState");
+                }
+                return m_type;
+            }
+        }
+        private static FieldInfo field_row
+        {
+            get
+            {
+                if (m_field_row == null)
+                {
+                    m_field_row = type.GetField("row", BindingFlags.Public | BindingFlags.Instance);
+                }
+                return m_field_row;
+            }
+        }
+        private static FieldInfo field_selectionChanged
+        {
+            get
+            {
+                if (m_field_selectionChanged == null)
+                {
+                    m_field_selectionChanged = type.GetField("selectionChanged", BindingFlags.Public | BindingFlags.Instance);
+                }
+                return m_field_selectionChanged;
+            }
+        }
+        private static FieldInfo field_totalRows
+        {
+            get
+            {
+                if (m_field_totalRows == null)
+                {
+                    m_field_totalRows = type.GetField("totalRows", BindingFlags.Public | BindingFlags.Instance);
+                }
+                return m_field_totalRows;
+            }
+        }
+        private static FieldInfo field_scrollPos
+        {
+            get
+            {
+                if (m_field_scrollPos == null)
+                {
+                    m_field_scrollPos = type.GetField("scrollPos", BindingFlags.Public | BindingFlags.Instance);
+                }
+                return m_field_scrollPos;
+            }
+        }
+        private static FieldInfo field_rowHeight
+        {
+            get
+            {
+                if (m_field_rowHeight == null)
+                {
+                    m_field_rowHeight = type.GetField("rowHeight", BindingFlags.Public | BindingFlags.Instance);
+                }
+                return m_field_rowHeight;
+            }
+        }
+        private static FieldInfo field_ID
+        {
+            get
+            {
+                if (m_field_ID == null)
+                {
+                    m_field_ID = type.GetField("ID", BindingFlags.Public | BindingFlags.Instance);
+                }
+                return m_field_ID;
+            }
+        }
 
         public ListViewState(int totalRows, int rowHeight)
         {
-            Assembly editorAsm = Assembly.GetAssembly(typeof(EditorGUILayout));
-            Type type = editorAsm.GetType("UnityEditor.ListViewState");
             obj = Activator.CreateInstance(type, totalRows, rowHeight);
-            _row = type.GetField("row", BindingFlags.Public | BindingFlags.Instance);
-            _selectionChanged = type.GetField("selectionChanged", BindingFlags.Public | BindingFlags.Instance);
-            _totalRows = type.GetField("totalRows", BindingFlags.Public | BindingFlags.Instance);
-            _scrollPos = type.GetField("scrollPos", BindingFlags.Public | BindingFlags.Instance);
-            _rowHeight = type.GetField("rowHeight", BindingFlags.Public | BindingFlags.Instance);
-            _ID = type.GetField("ID", BindingFlags.Public | BindingFlags.Instance);
         }
         public int row
         {
             get
             {
-                return (int)_row.GetValue(obj);
+                return (int)field_row.GetValue(obj);
             }
             set
             {
-                _row.SetValue(obj, value);
+                field_row.SetValue(obj, value);
             }
         }
         public bool selectionChanged
         {
             get
             {
-                return (bool)_selectionChanged.GetValue(obj);
+                return (bool)field_selectionChanged.GetValue(obj);
             }
             set
             {
-                _selectionChanged.SetValue(obj, value);
+                field_selectionChanged.SetValue(obj, value);
             }
         }
         public int totalRows
         {
             get
             {
-                return (int)_totalRows.GetValue(obj);
+                return (int)field_totalRows.GetValue(obj);
             }
             set
             {
-                _totalRows.SetValue(obj, value);
+                field_totalRows.SetValue(obj, value);
             }
         }
         public Vector2 scrollPos
         {
             get
             {
-                return (Vector2)_scrollPos.GetValue(obj);
+                return (Vector2)field_scrollPos.GetValue(obj);
             }
             set
             {
-                _scrollPos.SetValue(obj, value);
+                field_scrollPos.SetValue(obj, value);
             }
         }
         public int rowHeight
         {
             get
             {
-                return (int)_rowHeight.GetValue(obj);
+                return (int)field_rowHeight.GetValue(obj);
             }
             set
             {
-                _rowHeight.SetValue(obj, value);
+                field_rowHeight.SetValue(obj, value);
             }
         }
         public int ID
         {
             get
             {
-                return (int)_ID.GetValue(obj);
+                return (int)field_ID.GetValue(obj);
             }
             set
             {
-                _ID.SetValue(obj, value);
+                field_ID.SetValue(obj, value);
             }
         }
     }
@@ -219,29 +294,71 @@ public class CustomConsoleWindow : EditorWindow
         [SerializeField]
         public object obj;
 
-        private FieldInfo _row;
-        private FieldInfo _column;
-        private FieldInfo _position;
+        private static Type m_type;
+        private static FieldInfo m_field_row;
+        private static FieldInfo m_field_column;
+        private static FieldInfo m_field_position;
+
+        private static Type type
+        {
+            get
+            {
+                if (m_type == null)
+                {
+                    Assembly editorAsm = Assembly.GetAssembly(typeof(EditorGUILayout));
+                    m_type = editorAsm.GetType("UnityEditor.ListViewElement");
+                }
+                return m_type;
+            }
+        }
+        private static FieldInfo field_row
+        {
+            get
+            {
+                if (m_field_row == null)
+                {
+                    m_field_row = type.GetField("row", BindingFlags.Public | BindingFlags.Instance);
+                }
+                return m_field_row;
+            }
+        }
+        private static FieldInfo field_column
+        {
+            get
+            {
+                if (m_field_column == null)
+                {
+                    m_field_column = type.GetField("column", BindingFlags.Public | BindingFlags.Instance);
+                }
+                return m_field_column;
+            }
+        }
+        private static FieldInfo field_position
+        {
+            get
+            {
+                if (m_field_position == null)
+                {
+                    m_field_position = type.GetField("position", BindingFlags.Public | BindingFlags.Instance);
+                }
+                return m_field_position;
+            }
+        }
 
         public ListViewElement(object obj)
         {
-            Assembly editorAsm = Assembly.GetAssembly(typeof(EditorGUILayout));
-            Type type = editorAsm.GetType("UnityEditor.ListViewElement");
             this.obj = obj;
-            _row = type.GetField("row", BindingFlags.Public | BindingFlags.Instance);
-            _column = type.GetField("column", BindingFlags.Public | BindingFlags.Instance);
-            _position = type.GetField("position", BindingFlags.Public | BindingFlags.Instance);
         }
 
         public int row
         {
             get
             {
-                return (int)_row.GetValue(obj);
+                return (int)field_row.GetValue(obj);
             }
             set
             {
-                _row.SetValue(obj, value);
+                field_row.SetValue(obj, value);
             }
         }
 
@@ -249,11 +366,11 @@ public class CustomConsoleWindow : EditorWindow
         {
             get
             {
-                return (int)_column.GetValue(obj);
+                return (int)field_column.GetValue(obj);
             }
             set
             {
-                _column.SetValue(obj, value);
+                field_column.SetValue(obj, value);
             }
         }
 
@@ -261,12 +378,13 @@ public class CustomConsoleWindow : EditorWindow
         {
             get
             {
-                return (Rect)_position.GetValue(obj);
+                return (Rect)field_position.GetValue(obj);
             }
             set
             {
-                _position.SetValue(obj, value);
+                field_position.SetValue(obj, value);
             }
         }
     }
+    #endregion
 }
